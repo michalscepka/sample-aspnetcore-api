a88e8bdb4788f1016b1c5dc1d811f7a370f7e30a
michalscepka <55493644+michalscepka@users.noreply.github.com>
2024-08-05 00:47:30 +0200

    Create domain User

---
+++ src/SampleApp.Domain/User.cs
@@ -0,0 +1,8 @@
+﻿namespace SampleApp.Domain;
+
+public class User
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public ushort YearOfBirth { get; set; }
+}
